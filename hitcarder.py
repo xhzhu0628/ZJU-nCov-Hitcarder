cbd2e540e53132ddd40978d4fe8b78c2fb1fb5b6
Xuhao Zhu <xhzhu@zju.edu.cn>
2022-05-08 00:22:47 +0800

    添加了识别验证码功能

--- hitcarder.py
+++ hitcarder.py
@@ -9,6 +9,7 @@ import datetime
 import os
 import sys
 import message
+import ddddocr
 
 
 class HitCarder(object):
@@ -29,16 +30,22 @@ class HitCarder(object):
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+
         self.sess = requests.Session()
         self.sess.keep_alive = False
         retry = Retry(connect=3, backoff_factor=0.5)
         adapter = HTTPAdapter(max_retries=retry)
         self.sess.mount('http://', adapter)
         self.sess.mount('https://', adapter)
+        self.cookie_dict = None
         # ua = UserAgent()
         # self.sess.headers['User-Agent'] = ua.chrome
         self.sess.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+
+        # 验证码部分
+        self.captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+        self.ocr = ddddocr.DdddOcr()
 
     def login(self):
         """Login to ZJU platform"""
@@ -60,7 +67,7 @@ class HitCarder(object):
         }
         time.sleep(1)
         res = self.sess.post(url=self.login_url, data=data)
-
+        self.cookie_dict =requests.utils.dict_from_cookiejar(self.sess.cookies)
         # check if login successfully
         if '统一身份认证' in res.content.decode():
             raise LoginError('登录失败，请核实账号密码重新登录')
@@ -139,6 +146,10 @@ class HitCarder(object):
         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
         new_info['sfqrxxss'] = 1 # 属实
 
+        #验证码识别
+        resp = self.sess.get(self.captcha_url)
+        captcha = self.ocr.classification(resp.content)
+        new_info['verifyCode'] = captcha
         self.info = new_info
         # print(json.dumps(self.info))
         return new_info
